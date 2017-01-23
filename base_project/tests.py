f03388eccf22fe2b1e8a40a1f0ce54be608e0dd8
Andres <androiz10@gmail.com>
2017-01-23 13:17:26 +0100

    - Test #1

--- base_project/tests.py
+++ base_project/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class TokenTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client(HTTP_HOST='testserver')
+
+    def test_create_user(self):
+        form = {
+            'name': 'Fred',
+            'surname': 'Weasly',
+            'email': 'fred.weasly@hp.hp',
+            'password': 'george'
+        }
+
+        response = self.client.post('/sign_up/', form)
+        fred = User.objects.get(email="fred.weasly@hp.hp")
+
+
+        self.assertRedirects(response, "/", status_code=302, target_status_code=200, host=None,
+                                       msg_prefix='', fetch_redirect_response=True)
+
+        self.assertEqual(fred.is_active, False)
